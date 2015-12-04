638839fedb7128a53fa58f148352a108b3a838c8
pachay <apachay@octoberry.ru>
2015-12-04 18:51:42 +0300

    ios mock

--- fridge/routes.py
+++ fridge/routes.py
@@ -1,6 +1,5 @@
 # coding=utf-8
 import json
-
 from flask import request
 from fridge.app import app, state
 from fridge.forms import ProductForm
@@ -13,53 +12,107 @@ def cart_items():
     form = ProductForm.from_json(data)
     if form.validate():
         print 'creating product'
-        return json.dumps({}), 200
+        return json.dumps({}), 200, {'Content-Type': 'application/json; charset=utf-8'}
     else:
-        raise json.dumps({'error': 'can not add product'}), 400
+        return json.dumps({'error': 'can not add product'}), 400, {'Content-Type': 'application/json; charset=utf-8'}
 
 
 @app.route('/cart/items', methods=['GET'])
 def cart_items_list():
-    return json.dumps({
-        "@type": "ProductListCardObject",
-        "id": "unique_id_1234",
-        "type": "product_list_raw",
-        "score": 100500,
-        "price_list": [
-            {
-                "@type": "PriceListPosition",
-                "id": "unique_item_id_1",
-                "price": 20000.0,
-                "img": "milk.png",
-                "description": "Отличное молоко от альпийских коров.",
-                "count": 3,
-                "currency":
-                    {
-                        "@type": "CurrencyObject",
-                        "name": "RUR",
-                        "readable_name": "Российский рубль",
-                        "minor_units": 100
-                    },
-                "short_description": "Супер-альп молоко. 1л."
-            },
-            {
-                "@type": "PriceListPosition",
-                "id": "unique_item_id_2",
-                "price": 10000.0,
-                "img": "sosison.png",
-                "description": "Сосисоны супер просто. Покупайте много!",
-                "count": 1,
-                "currency":
-                    {
-                        "@type": "CurrencyObject",
-                        "name": "RUR",
-                        "readable_name": "Российский рубль",
-                        "minor_units": 100
-                    },
-                "short_description": "Сосисоны. 10шт."
-            }
-        ]
-    }), 200, {'Content-Type': 'application/json; charset=utf-8'}
+    shop = request.args.get('shop', None)
+    if shop:
+        return json.dumps({
+            "@tfype": "ProductListCardObject",
+            "id": "unique_id_1234",
+            "type": "product_list_formed",
+            "score": 100500,
+            "storeItems": [
+                {
+                    "storeId": "storeId",
+                    "storeName": "Утконос",
+                    "price_list": [
+                        {
+                            "@type": "PriceListPosition",
+                            "id": "unique_item_id_1",
+                            "price": 20000.0,
+                            "img": "milk.png",
+                            "description": "Отличное молоко от альпийских коров.",
+                            "count": 3,
+                            "currency":
+                                {
+                                    "@type": "CurrencyObject",
+                                    "name": "RUR",
+                                    "readable_name": "Российский рубль",
+                                    "minor_units": 100
+                                },
+                            "short_description": "Супер-альп молоко. 1л."
+                        },
+                        {
+                            "@type": "PriceListPosition",
+                            "id": "unique_item_id_2",
+                            "price": 10000.0,
+                            "img": "sosison.png",
+                            "description": "Сосисоны супер просто. Покупайте много!",
+                            "count": 1,
+                            "currency":
+                                {
+                                    "@type": "CurrencyObject",
+                                    "name": "RUR",
+                                    "readable_name": "Российский рубль",
+                                    "minor_units": 100
+                                },
+                            "short_description": "Сосисоны. 10шт."
+                        }
+                    ]
+                },
+                {
+                    "storeId": "storeId",
+                    "storeName": "Утконос",
+                    "price_list": []
+                }
+            ]
+        }), 200, {'Content-Type': 'application/json; charset=utf-8'}
+    else:
+        return json.dumps({
+            "@type": "ProductListCardObject",
+            "id": "unique_id_1234",
+            "type": "product_list_raw",
+            "score": 100500,
+            "price_list": [
+                {
+                    "@type": "PriceListPosition",
+                    "id": "unique_item_id_1",
+                    "price": 20000.0,
+                    "img": "milk.png",
+                    "description": "Отличное молоко от альпийских коров.",
+                    "count": 3,
+                    "currency":
+                        {
+                            "@type": "CurrencyObject",
+                            "name": "RUR",
+                            "readable_name": "Российский рубль",
+                            "minor_units": 100
+                        },
+                    "short_description": "Супер-альп молоко. 1л."
+                },
+                {
+                    "@type": "PriceListPosition",
+                    "id": "unique_item_id_2",
+                    "price": 10000.0,
+                    "img": "sosison.png",
+                    "description": "Сосисоны супер просто. Покупайте много!",
+                    "count": 1,
+                    "currency":
+                        {
+                            "@type": "CurrencyObject",
+                            "name": "RUR",
+                            "readable_name": "Российский рубль",
+                            "minor_units": 100
+                        },
+                    "short_description": "Сосисоны. 10шт."
+                }
+            ]
+        }), 200, {'Content-Type': 'application/json; charset=utf-8'}
 
 
 @app.route('/cart/item/<string:item_id>', methods=['GET', 'DELETE'])
